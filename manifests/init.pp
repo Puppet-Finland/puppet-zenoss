704174fc4fb55181e4fc452703d0499e87862291
Robert Pearce <r.pearce@gns.cri.nz>
2013-10-23 13:47:17 +1300

    added ability to override the interface used for ip address - useful for machines with zenoss contacts on a difering interface

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,16 @@
 class zenoss {
     class client {
+        $zenoss_interface = hiera('zenoss::interface', undef)
+        
+        $ip_fact_for_zenoss = $zenoss_interface ? {
+            undef   => 'ipaddress',
+            default => "ipaddress_${zenoss_interface}",
+        }
+
         @@zenoss_host { "$fqdn":
             #ensure => present,
             alias => "$hostname",
-            ip => "$ipaddress",
+            ip => inline_template("<%= scope.lookupvar('$ip_fact_for_zenoss') %>"),
             zenosstype => "$kernel",
             zenosscollector => "$zenosscollector",
             serialnumber => "$serialnumber",
@@ -15,7 +22,7 @@ class zenoss {
     class server {
         $zenoss_api_user = hiera('zenoss::zenoss_api_user')
         $zenoss_api_password = hiera('zenoss:zenoss_api_password')
-        $zenoss_host_server = hiera('zenoss::zenoss_host_server,"zenoss.${domain}")
+        $zenoss_host_server = hiera('zenoss::zenoss_host_server',"zenoss.${domain}")
         Zenoss_host <<| |>> {
             zenossuri => "http://${zenoss_api_user}:${zenoss_api_password}@${zenoss_host_server}:8080/zport/dmd"
         }
